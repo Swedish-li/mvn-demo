f8163268df967669474f1c411c057a89c9b92607
Swedish-li <one.piece.li.2016@gmail.com>
2017-07-21 09:16:12 +0800

    代码风格修正

--- src/main/java/com/lrs/App.java
+++ src/main/java/com/lrs/App.java
@@ -1,31 +1,45 @@
 package com.lrs;
 
 /**
- * Hello world!
+ * Hello world.
  */
 public final class App {
 
+    /**
+     * 私有构造器.
+     */
     private App() {
     }
 
-    public static App getInstance(){
-    	return new App();
+    /**
+     * 获取实例.
+     *
+     * @return
+     *      app
+     */
+    public static App getInstance() {
+        return new App();
     }
 
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello World!");
     }
 
-    public void coverageTestMethod(String arg){
-    	System.out.println("Welcome to Java's World!");
-    	if ("ok".equals(arg)) {
-    		System.out.println("ok!");
-    	} else {
-    		System.out.println("arg is not ok!");
-    	}
+    /**
+     * 覆盖率测试方法.
+     *
+     * @param arg 状态参数
+     */
+    public void coverageTestMethod(final String arg) {
+        System.out.println("Welcome to Java's World!");
+        if ("ok".equals(arg)) {
+            System.out.println("ok!");
+        } else {
+            System.out.println("arg is not ok!");
+        }
     }
 }
